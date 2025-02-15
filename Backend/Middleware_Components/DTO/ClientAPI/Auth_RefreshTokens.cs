d61536e90fc48cdceefbd11beac4aab04315bf0e
StudyDevW <122658763+StudyDevW@users.noreply.github.com>
2025-02-15 08:10:25 +0400

    Добавил сессии, обновление токенов

---
+++ Backend/Middleware_Components/DTO/ClientAPI/Auth_RefreshTokens.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Middleware_Components.DTO.ClientAPI
+{
+    public class Auth_RefreshTokens
+    {
+        public string refreshToken { get; set; }
+    }
+}
