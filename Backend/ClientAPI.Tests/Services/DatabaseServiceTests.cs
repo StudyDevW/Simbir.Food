e1a5df3d983bbfef31f80c6e303f94fdb96f2c8d
diamond <diamond@DESKTOP-C0LRN55>
2025-02-19 11:51:34 +0400

    Рефакторинг тестов RegisterUser под стабы

--- Backend/ClientAPI.Tests/Services/DatabaseServiceTests.cs
+++ Backend/ClientAPI.Tests/Services/DatabaseServiceTests.cs
@@ -1,4 +1,4 @@
-﻿using ClientAPI.Database;
+﻿using ClientAPI.Services;
 using FluentAssertions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -12,14 +12,19 @@ using TestsBaseLib.Base;
 
 namespace ClientAPI.Tests.Services;
 
-public class DatabaseSDKTests : BaseTest
+public class DatabaseServiceTests : BaseTest
 {
     [Fact]
     public async Task RegisterUser_WithCorrentData_AddedUserInDb()
     {
         // arrange
-        var configuration = Mock.Of<IConfiguration>();
-        var context = await GetDbContext();
+        var context = new Mock<DataContext>();
+        var users = new List<UserTable>();
+
+        context.Setup(x => x.userTable).ReturnsDbSet(users);
+        context.Setup(x => x.userTable.Add(It.IsAny<UserTable>()))
+            .Callback<UserTable>(x => users.Add(x));
+
         var dto = new AuthSignUp
         {
             address = "Addr",
@@ -29,56 +34,73 @@ public class DatabaseSDKTests : BaseTest
             password = "test123",
             phone_number = "79278567486"
         };
-        var sut = new DatabaseSDK(configuration, context);
+
+        var sut = new DatabaseService(context.Object);
 
         // act
         await sut.RegisterUser(dto);
 
         // assert
-        var user = await context.userTable.FirstOrDefaultAsync();
+        var user = users.FirstOrDefault();
         user.Should().NotBeNull();
         user.email.Should().Be("test@gmail.com");
         user.password.Should().NotBe(dto.password);
     }
 
     [Fact]
-    public async Task RegisterUser_EmptyDto_CallsError()
+    public async Task RegisterUser_WithExistingUser_ThrowsException()
     {
         // arrange
-        var configuration = Mock.Of<IConfiguration>();
-        var context = await GetDbContext();
-        var dto = new AuthSignUp();
-        var sut = new DatabaseSDK(configuration, context);
+        var context = new Mock<DataContext>();
+
+        var login = "tested";
+        var users = new List<UserTable> 
+        {
+            new UserTable { login = login }
+        };
+
+        context.Setup(x => x.userTable).ReturnsDbSet(users);
+        context.Setup(x => x.userTable.Add(It.IsAny<UserTable>()))
+            .Callback<UserTable>(x => users.Add(x));
+
+        var dto = new AuthSignUp
+        {
+            address = "Addr",
+            email = "test@gmail.com",
+            login = login,
+            name = "Dominik",
+            password = "test123",
+            phone_number = "79278567486"
+        };
+
+        var sut = new DatabaseService(context.Object);
+
+        // act
+        Func<Task> act = async() => await sut.RegisterUser(dto);
+
+        // assert
+        await act.Should().ThrowAsync<Exception>().WithMessage("login already exist");
+    }
+
+    [Fact]
+    public async Task RegisterUser_NullDto_ThrowsException()
+    {
+        // arrange
+        var context = Mock.Of<DataContext>();
+        AuthSignUp dto = null;
+        var sut = new DatabaseService(context);
 
         // act
         Func<Task> act = async () => await sut.RegisterUser(dto);
 
         // assert
-        await act.Should().ThrowAsync<ArgumentNullException>();
-    }
-
-    [Fact]
-    public async Task RegisterUser_NullDto_UserTableStayEmpty()
-    {
-        // arrange
-        var configuration = Mock.Of<IConfiguration>();
-        var context = await GetDbContext();
-        AuthSignUp dto = null;
-        var sut = new DatabaseSDK(configuration, context);
-
-        // act
-        await sut.RegisterUser(dto);
-
-        // assert
-        var user = await context.userTable.CountAsync();
-        user.Should().Be(0);
+        await act.Should().ThrowAsync<Exception>().WithMessage("dto null");
     }
 
     [Fact]
     public void CheckUser_WithCorrectData_ReturnSuccess()
     {
         // arrange
-        var configuration = Mock.Of<IConfiguration>();
         var contextStub = new Mock<DataContext>();
 
         var login = "login123";
@@ -98,7 +120,7 @@ public class DatabaseSDKTests : BaseTest
 
         contextStub.Setup(x => x.userTable).ReturnsDbSet(list);
 
-        var sut = new DatabaseSDK(configuration, contextStub.Object);
+        var sut = new DatabaseService( contextStub.Object);
 
         var dto = new AuthSignIn
         {
@@ -111,7 +133,7 @@ public class DatabaseSDKTests : BaseTest
 
         // assert
         result.check_success.Should().NotBeNull();
-        result.check_success.username.Should().Be(login);
+        result.check_success.login.Should().Be(login);
         result.check_error.Should().BeNull();
     }
 
@@ -119,9 +141,8 @@ public class DatabaseSDKTests : BaseTest
     public void CheckUser_NullDto_ReturnError()
     {
         // arrange
-        var configuration = Mock.Of<IConfiguration>();
         var contextStub = new Mock<DataContext>();
-        var sut = new DatabaseSDK(configuration, contextStub.Object);
+        var sut = new DatabaseService(contextStub.Object);
         AuthSignIn dto = null;
 
         // act
@@ -136,9 +157,8 @@ public class DatabaseSDKTests : BaseTest
     public void CheckUser_UserDoesntExist_ReturnError()
     {
         // arrange
-        var configuration = Mock.Of<IConfiguration>();
         var contextStub = new Mock<DataContext>();
-        var sut = new DatabaseSDK(configuration, contextStub.Object);
+        var sut = new DatabaseService(contextStub.Object);
         var dto = new AuthSignIn
         {
             login = "login123",
@@ -158,7 +178,6 @@ public class DatabaseSDKTests : BaseTest
     public void CheckUser_WithWrongPassword_ReturnError()
     {
         // arrange
-        var configuration = Mock.Of<IConfiguration>();
         var contextStub = new Mock<DataContext>();
 
         var login = "login123";
@@ -178,7 +197,7 @@ public class DatabaseSDKTests : BaseTest
 
         contextStub.Setup(x => x.userTable).ReturnsDbSet(list);
 
-        var sut = new DatabaseSDK(configuration, contextStub.Object);
+        var sut = new DatabaseService(contextStub.Object);
 
         var dto = new AuthSignIn
         {
