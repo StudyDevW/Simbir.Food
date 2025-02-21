140a83814b8363b5e6c17ab8cc15207b21c7f836
diamond <diamond@DESKTOP-C0LRN55>
2025-02-21 19:51:47 +0400

    Сделал все интеграционные тесты для ClientServiceTests

--- Backend/ClientAPI.Tests/Services/ClientServiceTests.cs
+++ Backend/ClientAPI.Tests/Services/ClientServiceTests.cs
@@ -1,20 +1,15 @@
 ﻿using ClientAPI.Interfaces;
 using ClientAPI.Services;
 using FluentAssertions;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Middleware_Components.DTO.ClientAPI;
-using Middleware_Components.JWT.DTO.CheckUsers;
 using Middleware_Components.Services;
 using Moq;
 using ORM_Components;
 using ORM_Components.DTO.ClientAPI;
 using ORM_Components.Tables;
 using StackExchange.Redis;
-using System.Text.Json;
-using Telegram.Bot.Types;
 using Telegram_Components.Interfaces;
 using TestsBaseLib.Base;
 
@@ -60,59 +55,6 @@ public class ClientServiceTests : BaseTest
         _context.reviewTable.RemoveRange(_context.reviewTable);
         _context.restaurantFoodItemsTable.RemoveRange(_context.restaurantFoodItemsTable);
         await _context.SaveChangesAsync();
-    }
-
-    private async Task<(UserTable user, Auth_PairTokens tokens)> AddUser(string login, string password)
-    {
-        var hasher = new PasswordHasher<PasswordAppUser>();
-
-        var user = new UserTable
-        {
-            login = login,
-            password = hasher.HashPassword(new PasswordAppUser { login = login }, password),
-            address = "Moscow",
-            email = "mis@gmail.com",
-            name = "dominik",
-            phone_number = "7590235",
-            roles = new string[] { "Client" },
-        };
-
-        _context.userTable.Add(user);
-        await _context.SaveChangesAsync();
-
-        var check = new Auth_CheckSuccess
-        {
-            Id = user.Id,
-            login = user.login,
-            roles = user.roles.ToList(),
-            telegramChatId = "958235235"
-        };
-
-        var db = _multiplexer.GetDatabase();
-
-        var accessToken = _jwt.JwtTokenCreation(check);
-        var refreshToken = _jwt.RefreshTokenCreation(check);
-
-        db.StringSet($"accessTokens_storage_{check.Id}", JsonSerializer.Serialize(accessToken), TimeSpan.FromMinutes(5));
-        db.StringSet($"refreshTokens_storage_{check.Id}", JsonSerializer.Serialize(refreshToken), TimeSpan.FromDays(7));
-
-        var sessionInit = new List<Session_Init>()
-        {
-            new Session_Init()
-            {
-                timeAdd = DateTime.UtcNow,
-                statusSession = "active",
-                tokenSession = accessToken
-            }
-        };
-
-        db.StringSet($"session_storage_storage_{check.Id}", JsonSerializer.Serialize(sessionInit), TimeSpan.FromDays(7));
-
-        return (user, new Auth_PairTokens
-        {
-            accessToken = accessToken,
-            refreshToken = refreshToken
-        });
     }
 
     [Fact]
@@ -173,7 +115,7 @@ public class ClientServiceTests : BaseTest
             telegram_chatid = "1241285295"
         };
 
-        var addedUser = await AddUser(login, password);
+        var addedUser = await AuthNewUser(login, password);
 
         // act
         var result = clientService.LoginClient(dto);
@@ -204,7 +146,7 @@ public class ClientServiceTests : BaseTest
         var login = "test1";
         var password = "pass1";
 
-        var addedUser = await AddUser(login, password);
+        var addedUser = await AuthNewUser(login, password);
 
         // act
         var result = await clientService.ClientSignOut("Bearer " + addedUser.tokens.accessToken);
@@ -233,7 +175,7 @@ public class ClientServiceTests : BaseTest
         var login = "test1";
         var password = "pass1";
 
-        var addedUser = await AddUser(login, password);
+        var addedUser = await AuthNewUser(login, password);
         var tokens = addedUser.tokens;
 
         // act
@@ -258,5 +200,181 @@ public class ClientServiceTests : BaseTest
         session.HasValue.Should().BeTrue();
         access.HasValue.Should().BeTrue();
         refresh.HasValue.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task ClientMeInfo_WithCorrectBearerKey_ReturnsClientInfo()
+    {
+        // arrange
+        await ClearDataBeforeTesting();
+
+        var clientService = new ClientService(_sender, _session, _database, _jwt, _cache);
+
+        var login = "test1";
+        var password = "pass1";
+
+        var addedUser = await AuthNewUser(login, password);
+        var tokens = addedUser.tokens;
+        var user = addedUser.user;
+        var bearerKey = "Bearer " + tokens.accessToken;
+
+        // act
+        var result = await clientService.ClientMeInfo(bearerKey);
+
+        // assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(user.Id);
+        result.login.Should().Be(user.login);
+    }
+
+    [Fact]
+    public async Task UpdateClientInfo_WithCorrectData_UpdatesClient()
+    {
+        // arrange
+        await ClearDataBeforeTesting();
+
+        var clientService = new ClientService(_sender, _session, _database, _jwt, _cache);
+
+        var login = "test1";
+        var password = "pass1";
+
+        var addedUser = await AuthNewUser(login, password);
+        var tokens = addedUser.tokens;
+        var user = addedUser.user;
+        var bearerKey = "Bearer " + tokens.accessToken;
+
+        var dto = new ClientUpdate
+        {
+            address = "Omsk",
+            avatarImage = "other image",
+            email = "other@mail.ru",
+            name = "othername",
+            password = "newpass1",
+            phone_number = "89532053253"
+        };
+
+        // act
+        await clientService.UpdateClientInfo(bearerKey, dto);
+
+        // assert
+
+        var updatedUser = await _context.userTable.FindAsync(user.Id);
+        updatedUser.Should().NotBeNull();
+        updatedUser.address.Should().Be(dto.address);
+        updatedUser.avatarImage.Should().Be(dto.avatarImage);
+        updatedUser.email.Should().Be(dto.email);
+        updatedUser.name.Should().Be(dto.name);
+        updatedUser.phone_number.Should().Be(dto.phone_number);
+
+        var hasher = new PasswordHasher<PasswordAppUser>();
+        var verify = hasher.VerifyHashedPassword(new PasswordAppUser { login = updatedUser.login, passwordHashed = updatedUser.password },
+            updatedUser.password, dto.password);
+
+        verify.Should().Be(PasswordVerificationResult.Success);
+    }
+
+    [Fact]
+    public async Task AllProfilesGet_WithCorrectData_ReturnsAllProfiles()
+    {
+        // arrange
+        await ClearDataBeforeTesting();
+
+        var clientService = new ClientService(_sender, _session, _database, _jwt, _cache);
+
+        var login = "test1";
+        var password = "pass1";
+
+        var addedUser = await AuthNewUser(login, password, "Admin");
+        var tokens = addedUser.tokens;
+        var user = addedUser.user;
+        var bearerKey = "Bearer " + tokens.accessToken;
+
+        for (int i = 1; i < 9; i++)
+            _context.userTable.Add(new UserTable
+            {
+                Id = Guid.NewGuid(),
+                login = $"user_{i}",
+                address = "testadd",
+                email = "test@gmail.com",
+                name = "test",
+                phone_number = "7590235",
+                roles = new string[] { "Client" },
+                password = "hash"
+            });
+
+        await _context.SaveChangesAsync();
+
+        // act
+        var clients = await clientService.AllProfilesGet(bearerKey, 2, 5);
+
+        // assert
+        clients.Should().NotBeNull();
+        clients.Content.Count.Should().Be(5);
+    }
+
+    [Fact]
+    public async Task CreateClientWithAdmin_WithCorrectData_AddsUserToDb()
+    {
+        // arrange
+        await ClearDataBeforeTesting();
+
+        var clientService = new ClientService(_sender, _session, _database, _jwt, _cache);
+
+        var dto = new ClientAdd_Admin
+        {
+            login = "newuser1",
+            password = "newuser1pass",
+            address = "Ufa",
+            email = "ufen@gmail.com",
+            name = "ufenic",
+            phone_number = "52363272",
+            telegram_chatid = "734834834",
+            roles = new string[] { "Client", "Admin" }
+        };
+
+        var addedAdmin = await AuthNewUser("admin1", "pass1", "Admin");
+        var bearer = "Bearer " + addedAdmin.tokens.accessToken;
+
+        // act
+        await clientService.CreateClientWithAdmin(bearer, dto);
+
+        // assert
+        var user = await _context.userTable.FirstOrDefaultAsync(x => x.login == dto.login);
+        user.roles.Should().BeEquivalentTo(dto.roles);
+        user.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task DeleteClientWithAdmin_WithCorrectData_DeletesUser()
+    {
+        // arrange
+        await ClearDataBeforeTesting();
+
+        var clientService = new ClientService(_sender, _session, _database, _jwt, _cache);
+
+        var addedAdmin = await AuthNewUser("admin1", "pass1", "Admin");
+        var bearer = "Bearer " + addedAdmin.tokens.accessToken;
+
+        var deleteableUser = await AddUserToDb("deluser1", "delpass1");
+
+        var restaurant = GenerateRestaurant(deleteableUser.Id);
+        var foods = GenerateFoodItems(restaurant.Id, 3);
+
+        _context.restaurantTable.Add(restaurant);
+        _context.restaurantFoodItemsTable.AddRange(foods);
+        await _context.SaveChangesAsync();
+
+        // act
+        await clientService.DeleteClientWithAdmin(bearer, deleteableUser.Id);
+
+        // assert
+        var user = await _context.userTable.FindAsync(deleteableUser.Id);
+        user.Should().BeNull();
+
+        var restaurantsLeft = await _context.restaurantTable.CountAsync();
+        restaurantsLeft.Should().Be(0);
+
+        var foodsLeft = await _context.restaurantFoodItemsTable.CountAsync();
+        foodsLeft.Should().Be(0);
     }
 }
--- Backend/TestsBaseLib/Base/BaseTest.cs
+++ Backend/TestsBaseLib/Base/BaseTest.cs
@@ -1,13 +1,20 @@
-﻿using ClientAPI.Interfaces;
+﻿using Bogus;
+using ClientAPI.Interfaces;
 using ClientAPI.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Middleware_Components.Cache;
+using Middleware_Components.DTO.ClientAPI;
 using Middleware_Components.JWT;
+using Middleware_Components.JWT.DTO.CheckUsers;
 using Middleware_Components.Services;
 using Moq;
 using ORM_Components;
+using ORM_Components.DTO.ClientAPI;
+using ORM_Components.Tables;
 using StackExchange.Redis;
+using System.Text.Json;
 
 namespace TestsBaseLib.Base;
 
@@ -63,5 +70,120 @@ public class BaseTest
         conf.Setup(x => x["Jwt:Audience"]).Returns("SimbirFoodUsers");
 
         return new JwtSDK(conf.Object, cache);
+    }
+
+    protected UserTable GenerateUser(string login, string passwordHash, string[] roles)
+    {
+        var faker = new Faker<UserTable>();
+        faker.RuleFor(x => x.Id, f => Guid.NewGuid())
+            .RuleFor(x => x.login, f => login)
+            .RuleFor(x => x.password, _ => passwordHash)
+            .RuleFor(x => x.name, f => f.Name.FirstName())
+            .RuleFor(x => x.email, (f, x) => f.Internet.Email(x.name))
+            .RuleFor(x => x.phone_number, f => f.Phone.PhoneNumber())
+            .RuleFor(x => x.address, f => f.Address.City())
+            .RuleFor(x => x.roles, _ => roles);
+
+        return faker.Generate();
+    }
+
+    protected RestaurantTable GenerateRestaurant(Guid user_id)
+    {
+        var faker = new Faker<RestaurantTable>();
+        faker.RuleFor(x => x.Id, _ => Guid.NewGuid())
+            .RuleFor(x => x.user_id, _ => user_id)
+            .RuleFor(x => x.phone_number, f => f.Phone.PhoneNumber())
+            .RuleFor(x => x.imagePath, f => f.Random.Word())
+            .RuleFor(x => x.restaurantName, f => f.Random.Word())
+            .RuleFor(x => x.description, f => f.Random.Words(10))
+            .RuleFor(x => x.address, f => f.Address.City())
+            .RuleFor(x => x.close_time, f => f.Date.Between(new DateTime(2016, 1, 1, 0, 0, 0, DateTimeKind.Utc), DateTime.UtcNow))
+            .RuleFor(x => x.open_time, (f, x) => f.Date.Between(DateTime.UtcNow, x.close_time))
+            .RuleFor(x => x.status, f => f.Random.Word());
+
+        return faker.Generate();
+    }
+
+    protected List<RestaurantFoodItemsTable> GenerateFoodItems(Guid restaurant_id, int count)
+    {
+        var faker = new Faker<RestaurantFoodItemsTable>();
+        faker.RuleFor(x => x.Id, _ => Guid.NewGuid())
+            .RuleFor(x => x.restaurant_id, _ => restaurant_id)
+            .RuleFor(x => x.price, f => f.Random.Number(200))
+            .RuleFor(x => x.weight, f => f.Random.Number(1, 100))
+            .RuleFor(x => x.calories, f => f.Random.Number(50, 3000))
+            .RuleFor(x => x.image, f => f.Random.Word())
+            .RuleFor(x => x.name, f => f.Random.Word());
+
+        return faker.Generate(count);
+    }
+
+    /// <summary>
+    /// !!! Только для интеграционных тестов !!!
+    /// Добавляет нового пользователя в БД
+    /// </summary>
+    protected async Task<UserTable> AddUserToDb(string login, string password, string role = "Client")
+    {
+        var hasher = new PasswordHasher<PasswordAppUser>();
+
+        var hash = hasher.HashPassword(new PasswordAppUser { login = login }, password);
+        var user = GenerateUser(login, hash, new string[] { role });
+
+        using (var _context = GetDbContext())
+        {
+            _context.userTable.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        return user;
+    }
+
+    /// <summary>
+    /// !!! Только для интеграционных тестов !!!
+    /// Добавляет нового пользователя в БД и авторизует его, добавляя токены в кэш Redis
+    /// </summary>
+    protected async Task<(UserTable user, Auth_PairTokens tokens)> AuthNewUser(string login, string password, string role = "Client")
+    {
+        var user = await AddUserToDb(login, password, role);
+
+        var check = new Auth_CheckSuccess
+        {
+            Id = user.Id,
+            login = user.login,
+            roles = user.roles.ToList(),
+            telegramChatId = "958235235"
+        };
+
+        using (var _multiplexer = GetConnectionMultiplexer())
+        {
+            var _cache = GetCacheService(_multiplexer);
+            var _jwt = GetJwtService(_cache);
+
+            var db = _multiplexer.GetDatabase();
+
+            var accessToken = _jwt.JwtTokenCreation(check);
+            var refreshToken = _jwt.RefreshTokenCreation(check);
+
+            db.StringSet($"accessTokens_storage_{check.Id}", JsonSerializer.Serialize(accessToken), TimeSpan.FromMinutes(5));
+            db.StringSet($"refreshTokens_storage_{check.Id}", JsonSerializer.Serialize(refreshToken), TimeSpan.FromDays(7));
+
+            var sessionInit = new List<Session_Init>()
+            {
+                new Session_Init()
+                {
+                    timeAdd = DateTime.UtcNow,
+                    statusSession = "active",
+                    tokenSession = accessToken
+                }
+            };
+
+            db.StringSet($"session_storage_storage_{check.Id}", JsonSerializer.Serialize(sessionInit), TimeSpan.FromDays(7));
+
+            return (user, new Auth_PairTokens
+            {
+                accessToken = accessToken,
+                refreshToken = refreshToken
+            });
+        }
     }
 }
