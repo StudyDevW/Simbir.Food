0d237abc4abb96e58ae484a5239d3d90eb64a0b1
Ivan Rozanov <rozanovivan13@gmail.com>
2025-02-21 20:40:36 +0400

    Фикс Order, промежуточное сохранение перед слиянием

---
+++ Backend/ORM_Components/DTO/RestaurantAPI/Order_DTO.cs
@@ -0,0 +1,15 @@
+﻿using ORM_Components.Tables.Helpers;
+
+namespace ORM_Components.DTO.RestaurantAPI
+{
+    public class Order_DTO
+    {
+        public Guid id { get; set; }
+        public Guid client_id { get; set; }
+        public Guid restaurant_id { get; set; }
+        public Guid? courier_id { get; set; }
+        public OrderStatus status { get; set; }
+        public int total_price { get; set; }
+        public DateTime order_date { get; set; }
+    }
+}
---
+++ Backend/RestaurantAPI/Model/Controllers/OrdersDeviationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Middleware_Components.Services;
+using ORM_Components.DTO.RestaurantAPI;
+
+namespace RestaurantAPI.Model.Controllers
+{
+    [Route("api/OrdersDeviation")]
+    [ApiController]
+    public class OrdersDeviationController : ControllerBase
+    {
+        private readonly IJwtService _jwtServices;
+
+        public OrdersDeviationController (IJwtService jwtServices)
+        {
+            _jwtServices = jwtServices;
+        }
+
+        [HttpPost("SentOrder")]
+        public async Task<IActionResult> SentOrder([FromBody] Order_DTO order_DTO)
+        {
+            if (order_DTO == null)
+            {
+                return BadRequest("Неверные данные заказа.");
+            }
+
+            string message = $"Ваш заказ в ресторане {order_DTO.restaurant_id} был отклонен.\n" +
+                             $"Сумма заказа: {order_DTO.total_price}.\n" +
+                             $"Дата заказа: {order_DTO.order_date}.";
+
+            await _telegramChatId.Send(order_DTO.client_id, message);
+
+            return Ok("Заказ отклонен и отправлено уведомление.");
+        }
+    }
+}
