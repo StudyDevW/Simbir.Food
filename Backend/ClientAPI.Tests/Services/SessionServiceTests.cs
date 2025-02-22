576f0862d983e415684ce72067b5dda8ea95086e
diamond <diamond@DESKTOP-C0LRN55>
2025-02-22 20:48:40 +0400

    Простые тесты для SessionService

---
+++ Backend/ClientAPI.Tests/Services/SessionServiceTests.cs
@@ -0,0 +1,106 @@
+﻿using ClientAPI.Services;
+using FluentAssertions;
+using Middleware_Components.DTO.ClientAPI;
+using ORM_Components.Tables;
+using System.Text.Json;
+using TestsBaseLib.Mocks;
+
+namespace ClientAPI.Tests.Services;
+
+public class SessionServiceTests
+{
+    [Fact]
+    public void SetupSession_WithCorrectDataAndFirstTimeCall_CreatesCacheSession()
+    {
+        // arrange
+        var cache = new DictCacheServiceMock();
+        var sut = new SessionService(cache);
+
+        var userId = Guid.NewGuid();
+        var token = "some_token";
+
+        // act
+        sut.SetupSession(userId, token);
+
+        // assert
+        var stringValue = cache.Dict[$"session_storage_storage_{userId}"];
+        var value = JsonSerializer.Deserialize<Session_Init[]>(stringValue);
+
+        value.Should().NotBeNull();
+        value.Count().Should().Be(1);
+        value.First().tokenSession.Should().Be(token);
+   }
+
+    [Fact]
+    public void SetupSession_WithCorrectDataAndSecondTimeCall_AddedTokenToOldCacheSession()
+    {
+        // arrange
+        var cache = new DictCacheServiceMock();
+        var sut = new SessionService(cache);
+
+        var userId = Guid.NewGuid();
+        var token = "some_token";
+        var second_token = "second_token";
+
+        // act
+        sut.SetupSession(userId, token);
+        sut.SetupSession(userId, second_token);
+
+        // assert
+        var stringValue = cache.Dict[$"session_storage_storage_{userId}"];
+        var value = JsonSerializer.Deserialize<Session_Init[]>(stringValue);
+
+        value.Should().NotBeNull();
+        value.Count().Should().Be(2);
+        value.First().tokenSession.Should().Be(token);
+        value.Last().tokenSession.Should().Be(second_token);
+
+        value.First().statusSession.Should().Be("expired");
+        value.Last().statusSession.Should().Be("active");
+        value.First().timeDel.Value.Should().BeLessThan(TimeSpan.FromSeconds(1)).Before(DateTime.UtcNow);
+    }
+
+    [Fact]
+    public void ClientSignOutSession_WithCorrectData_AllActiveSessionsHaveToBeExpired()
+    {
+        // arrange
+        var cache = new DictCacheServiceMock();
+        var sut = new SessionService(cache);
+
+        var userId = Guid.NewGuid();
+        var token = "some_token";
+
+        // act
+        sut.SetupSession(userId, token);
+        sut.ClientSignOutSession(userId);
+
+        // assert
+        var stringValue = cache.Dict[$"session_storage_storage_{userId}"];
+        var value = JsonSerializer.Deserialize<Session_Init[]>(stringValue);
+
+        value.All(x => x.statusSession == "expired").Should().BeTrue();
+    }
+
+    [Fact]
+    public void RefreshSession_WithCorrectDataAndSecondTimeCall_AddedTokenToOldCacheSession()
+    {
+        // arrange
+        var cache = new DictCacheServiceMock();
+        var sut = new SessionService(cache);
+
+        var userId = Guid.NewGuid();
+        var token = "some_token";
+        var new_token = "second_token";
+
+        // act
+        sut.SetupSession(userId, token);
+        sut.RefreshSession(userId, new_token);
+
+        // assert
+        var stringValue = cache.Dict[$"session_storage_storage_{userId}"];
+        var value = JsonSerializer.Deserialize<Session_Init[]>(stringValue);
+
+        value.First().timeUpd.Value.Should().BeLessThan(TimeSpan.FromSeconds(1)).Before(DateTime.UtcNow);
+        value.First().tokenSession.Should().Be(new_token);
+    }
+}
---
+++ Backend/TestsBaseLib/Mocks/DictCacheServiceMock.cs
@@ -0,0 +1,121 @@
+﻿using Middleware_Components.Services;
+using System.Text.Json;
+
+namespace TestsBaseLib.Mocks;
+
+public class DictCacheServiceMock : ICacheService
+{
+    private readonly Dictionary<string, string> _dict;
+
+    public IReadOnlyDictionary<string, string> Dict
+    {
+        get => _dict;
+    }
+
+    public DictCacheServiceMock()
+    {
+        _dict = new Dictionary<string, string>();
+    }
+
+    public bool CheckExistKeysStorage<T>(Guid id_user, string type)
+    {
+        var cache_data = GetData<T>($"{type}_storage_{id_user}");
+
+        if (cache_data != null)
+            return true;
+
+        return false;
+    }
+
+    public bool CheckExistKeysStorage(Guid id_user, string type)
+    {
+        var cache_data = GetData<string>($"{type}_storage_{id_user}");
+
+        if (cache_data != null)
+            return true;
+
+        return false;
+    }
+
+    public bool CheckExistKeysStorage<T>(string storage_desc)
+    {
+        var cache_data = GetData<T>($"{storage_desc}_storage");
+
+        if (cache_data != null)
+            return true;
+
+        return false;
+    }
+
+    public void DeleteKeyFromStorage(Guid id_user, string type)
+    {
+        RemoveData($"{type}_storage_{id_user}");
+    }
+
+    public void DeleteKeyFromStorage(string storage_desc)
+    {
+        RemoveData($"{storage_desc}_storage");
+    }
+
+    public T GetData<T>(string key)
+    {
+        var result = _dict.TryGetValue(key, out var data);
+
+        if (!result)
+            return default;
+
+        return JsonSerializer.Deserialize<T>(data);
+    }
+
+    public string? GetKeyFromStorage(Guid id_user, string type)
+    {
+        var cache_data = GetData<string>($"{type}_storage_{id_user}");
+
+        return cache_data;
+    }
+
+    public T GetKeyFromStorage<T>(Guid id_user, string type)
+    {
+        var cache_data = GetData<T>($"{type}_storage_{id_user}");
+
+        return cache_data;
+    }
+
+    public T GetKeyFromStorage<T>(string storage_desc)
+    {
+        var cache_data = GetData<T>($"{storage_desc}_storage");
+
+        return cache_data;
+    }
+
+    public object RemoveData(string key)
+    {
+        var exists = _dict.TryGetValue(key, out var _);
+
+        if (exists)
+            _dict.Remove(key);
+
+        return exists;
+    }
+
+    public bool SetData<T>(string key, T value, DateTimeOffset expirationTime)
+    {
+        _dict[key] = JsonSerializer.Serialize(value);
+        return true;
+    }
+
+    public void WriteKeyInStorage(Guid id_user, string type, string key, DateTime extime)
+    {
+        SetData($"{type}_storage_{id_user}", key, extime);
+    }
+
+    public void WriteKeyInStorage<T>(Guid id_user, string type, T key, DateTime extime)
+    {
+        SetData($"{type}_storage_{id_user}", key, extime);
+    }
+
+    public void WriteKeyInStorageObject<T>(string storage_desc, T key, DateTime extime)
+    {
+        SetData($"{storage_desc}_storage", key, extime);
+    }
+}
