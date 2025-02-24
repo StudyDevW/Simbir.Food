4e0ea80f8bbab9f3b0c8f9ec3211c2d70f41f449
Артём Алейкин <aleyckin@mail.ru>
2025-02-24 16:35:53 +0400

    Кролик заработал

---
+++ Backend/ORM_Components/DTO/RestaurantAPI/OrderDtos.cs
@@ -0,0 +1,6 @@
+﻿using ORM_Components.Tables.Helpers;
+
+namespace ORM_Components.DTO.RestaurantAPI
+{
+    public record OrderIdsDto(Guid Id, Guid clientId, Guid restaurant_id, Guid courier_id) { }
+}
