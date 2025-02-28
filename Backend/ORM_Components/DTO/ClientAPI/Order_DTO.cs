783e98b77d86afca71c6846ee20e45e2f17920f9
Ivan Rozanov <rozanovivan13@gmail.com>
2025-02-28 13:26:36 +0400

    Бета версия логики ресторана

--- Backend/ORM_Components/DTO/ClientAPI/Order_DTO.cs
+++ Backend/ORM_Components/DTO/ClientAPI/Order_DTO.cs
@@ -7,7 +7,7 @@ namespace ORM_Components.DTO.ClientAPI
         public Guid id { get; set; }
         public Guid client_id { get; set; }
         public Guid restaurant_id { get; set; }
-        public Guid? courier_id { get; set; }
+        public Guid courier_id { get; set; }
         public OrderStatus status { get; set; }
         public int total_price { get; set; }
         public DateTime order_date { get; set; }
