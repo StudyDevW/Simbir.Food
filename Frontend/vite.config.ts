75c1dca768a191b73f3785e5623843c381b6199c
StudyDevW <122658763+StudyDevW@users.noreply.github.com>
2026-03-31 05:58:57 +0400

    Все зависимости Telegram* переделал под VK (Backend)

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['impressively-confident-puffin.cloudpub.ru'],
     watch: {
       usePolling: true,
     },
